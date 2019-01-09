f77df31fb6186f2015150cc5b0470fb4083669b7
jjbboox <46516821+jjbboox@users.noreply.github.com>
2019-01-09 17:33:48 +0800

    Create ws2812.c

---
+++ src/ws2812.c
@@ -0,0 +1,71 @@
+#include <reg51.h>
+#include "ws2812.h"
+
+void Delay50us()		//@11.0592MHz
+{
+	unsigned char i, j;
+
+	_nop_();
+	i = 1;
+	j = 134;
+	do
+	{
+		while (--j);
+	} while (--i);
+}
+
+// RESET信号
+void reset() {
+	DOUT = 0;
+	Delay50us();
+}
+
+void send_p(Pixel _p) {
+	unsigned char p_v;
+	p_v = (_p.green * _p.brightness) >> 8;
+	SEND_BIT(p_v & 0x80);
+	SEND_BIT(p_v & 0x40);
+	SEND_BIT(p_v & 0x20);
+	SEND_BIT(p_v & 0x10);
+	SEND_BIT(p_v & 0x08);
+	SEND_BIT(p_v & 0x04);
+	SEND_BIT(p_v & 0x02);
+	SEND_BIT(p_v & 0x01);
+	p_v = (_p.red * _p.brightness) >> 8;
+	SEND_BIT(p_v & 0x80);
+	SEND_BIT(p_v & 0x40);
+	SEND_BIT(p_v & 0x20);
+	SEND_BIT(p_v & 0x10);
+	SEND_BIT(p_v & 0x08);
+	SEND_BIT(p_v & 0x04);
+	SEND_BIT(p_v & 0x02);
+	SEND_BIT(p_v & 0x01);
+	p_v = (_p.blue * _p.brightness) >> 8;
+	SEND_BIT(p_v & 0x80);
+	SEND_BIT(p_v & 0x40);
+	SEND_BIT(p_v & 0x20);
+	SEND_BIT(p_v & 0x10);
+	SEND_BIT(p_v & 0x08);
+	SEND_BIT(p_v & 0x04);
+	SEND_BIT(p_v & 0x02);
+	SEND_BIT(p_v & 0x01);
+}
+
+void send_pixels(Pixel *_p_list, unsigned int _start, unsigned int _cnt) {
+	Pixel *_p = &_p_list[_start % _cnt];
+	reset();
+	while(_cnt--) {
+		send_p(*_p);
+		_p = &_p_list[_p->next_pixel];
+	}
+	reset();
+}
+
+void set_pixels_color(Pixel *pixels, unsigned int _cnt, Pixel _color) {
+	int i;
+	for(i = 0; i < _cnt; i++) {
+		pixels[i].green = _color.green;
+		pixels[i].red = _color.red;
+		pixels[i].blue = _color.blue;
+	}
+}
