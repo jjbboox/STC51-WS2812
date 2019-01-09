933926710ad0e4ce8d07ee5172e4764cb93c50f7
jjbboox <46516821+jjbboox@users.noreply.github.com>
2019-01-09 17:15:35 +0800

    Create a void main.c

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <reg51.h>
+#include "ws2812.h"
+
+void setup();
+void loop();
+
+void main() {
+  setup();
+  
+  while(1) {
+    loop();
+  }
+}
+
+void setup() {
+  // TODO:
+}
+
+void loop() {
+  // TODO:
+}
